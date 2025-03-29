c5e2ec5244448b5944f45ba0b9e6dca55a6446d6
amaxi <attila.maxi@gmail.com>
2025-03-29 09:25:33 +0100

    add some fixes fro led

--- leds_off.sh
+++ leds_off.sh
@@ -4,5 +4,8 @@ echo 0 > /sys/class/leds/green:system/brightness
 echo 0 > /sys/class/leds/green:wlan2g/brightness
 echo 0 > /sys/class/leds/green:wlan5g/brightness
 echo 0 > /sys/class/leds/ath9k-phy0/brightness
+echo 0 >  /sys/class/leds/green:usb1/brightness
+echo 0 >  /sys/class/leds/green:usb2/brightness
+echo 0 >  /sys/class/leds/green:qss/brightness
 
 exit 0
