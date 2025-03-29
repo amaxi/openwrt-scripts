479f59f3f668c50468fb839fc729ea2142316e4e
amaxi <attila.maxi@gmail.com>
2025-03-29 09:19:11 +0100

    fix led path

--- leds_off.sh
+++ leds_off.sh
@@ -1,14 +1,8 @@
 #!/bin/ash
 
-echo 0 > /sys/class/leds/tp-link:green:system/brightness
-echo 0 > /sys/class/leds/tp-link:green:wan/brightness
-echo 0 > /sys/class/leds/tp-link:green:lan1/brightness
-echo 0 > /sys/class/leds/tp-link:green:lan2/brightness
-echo 0 > /sys/class/leds/tp-link:green:lan3/brightness
-echo 0 > /sys/class/leds/tp-link:green:lan4/brightness
-echo 0 > /sys/class/leds/tp-link:green:qss/brightness
-echo 0 > /sys/class/leds/tp-link:green:wlan2g/brightness
+echo 0 > /sys/class/leds/green:system/brightness
+echo 0 > /sys/class/leds/green:wlan2g/brightness
+echo 0 > /sys/class/leds/green:wlan5g/brightness
 echo 0 > /sys/class/leds/ath9k-phy0/brightness
-echo 0 > /sys/class/leds/ath9k-phy1/brightness
 
 exit 0
