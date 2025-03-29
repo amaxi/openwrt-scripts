e7b7031769479dd743e6fcff31b25d4860d2c714
amaxi <attila.maxi@gmail.com>
2025-03-29 09:11:03 +0100

    fix led path

--- leds_off.sh
+++ leds_off.sh
@@ -1,13 +1,13 @@
 #!/bin/ash
 
-echo 0 > /sys/class/leds/tp-link:blue:system/brightness
-echo 0 > /sys/class/leds/tp-link:blue:wan/brightness
-echo 0 > /sys/class/leds/tp-link:blue:lan1/brightness
-echo 0 > /sys/class/leds/tp-link:blue:lan2/brightness
-echo 0 > /sys/class/leds/tp-link:blue:lan3/brightness
-echo 0 > /sys/class/leds/tp-link:blue:lan4/brightness
-echo 0 > /sys/class/leds/tp-link:blue:qss/brightness
-echo 0 > /sys/class/leds/tp-link:blue:wlan2g/brightness
+echo 0 > /sys/class/leds/tp-link:green:system/brightness
+echo 0 > /sys/class/leds/tp-link:green:wan/brightness
+echo 0 > /sys/class/leds/tp-link:green:lan1/brightness
+echo 0 > /sys/class/leds/tp-link:green:lan2/brightness
+echo 0 > /sys/class/leds/tp-link:green:lan3/brightness
+echo 0 > /sys/class/leds/tp-link:green:lan4/brightness
+echo 0 > /sys/class/leds/tp-link:green:qss/brightness
+echo 0 > /sys/class/leds/tp-link:green:wlan2g/brightness
 echo 0 > /sys/class/leds/ath9k-phy0/brightness
 echo 0 > /sys/class/leds/ath9k-phy1/brightness
 
--- leds_on.sh
+++ leds_on.sh
@@ -1,13 +1,13 @@
 #!/bin/ash
 
-echo 1 > /sys/class/leds/tp-link:blue:system/brightness #255
-echo 1 > /sys/class/leds/tp-link:blue:wan/brightness
-echo 1 > /sys/class/leds/tp-link:blue:lan1/brightness
-echo 1 > /sys/class/leds/tp-link:blue:lan2/brightness
-echo 1 > /sys/class/leds/tp-link:blue:lan3/brightness
-echo 1 > /sys/class/leds/tp-link:blue:lan4/brightness
-echo 1 > /sys/class/leds/tp-link:blue:qss/brightness
-echo 1 > /sys/class/leds/tp-link:blue:wlan2g/brightness #255
+echo 1 > /sys/class/leds/tp-link:green:system/brightness #255
+echo 1 > /sys/class/leds/tp-link:green:wan/brightness
+echo 1 > /sys/class/leds/tp-link:green:lan1/brightness
+echo 1 > /sys/class/leds/tp-link:green:lan2/brightness
+echo 1 > /sys/class/leds/tp-link:green:lan3/brightness
+echo 1 > /sys/class/leds/tp-link:green:lan4/brightness
+echo 1 > /sys/class/leds/tp-link:green:qss/brightness
+echo 1 > /sys/class/leds/tp-link:green:wlan2g/brightness #255
 echo 1 > /sys/class/leds/ath9k-phy0/brightness #255
 echo 1 > /sys/class/leds/ath9k-phy1/brightness
 
